aa0732be0db506f6c437dbdd25d5354b872e9bf4
BenjaminDls <benjamin.danlos@live.fr>
2023-12-01 19:16:35 +0100

    add swagger

--- src/main/java/fr/benjamindanlos/laptimes/Controller/LaptimeController.java
+++ src/main/java/fr/benjamindanlos/laptimes/Controller/LaptimeController.java
@@ -5,6 +5,12 @@ import fr.benjamindanlos.laptimes.Entities.Laptime;
 import fr.benjamindanlos.laptimes.Exceptions.BadRequestException;
 import fr.benjamindanlos.laptimes.Exceptions.NotFoundException;
 import fr.benjamindanlos.laptimes.Repository.LaptimeRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,6 +26,30 @@ public class LaptimeController
 	@Autowired
 	private LaptimeRepository laptimeRepository;
 
+	@Operation(description = "Search laptimes from the name of a driver",
+		requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+			content = @Content(
+				contentSchema = @Schema(
+					implementation = SearchDTO.class
+				)
+			)
+		),
+		responses = {
+			@ApiResponse(responseCode = "200", 
+				description = "Matching laptimes",
+				content = @Content(
+					array = @ArraySchema(
+						contains = @Schema(
+							implementation = Laptime.class
+						)
+					)
+				)
+			),
+			@ApiResponse(responseCode = "400",
+				description = "Invalid request"
+			)
+		}
+	)
 	@PostMapping("/bestByDriver")
 	public ResponseEntity<List<Laptime>> getAllBestLaptimesByDriver(@RequestBody SearchDTO body)
 	{
@@ -33,6 +63,30 @@ public class LaptimeController
 		return ResponseEntity.ok().body(results);
 	}
 
+	@Operation(description = "Search laptimes from the name of a track",
+		requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+			content = @Content(
+				contentSchema = @Schema(
+					implementation = SearchDTO.class
+				)
+			)
+		),
+		responses = {
+			@ApiResponse(responseCode = "200",
+				description = "Matching laptimes",
+				content = @Content(
+					array = @ArraySchema(
+						contains = @Schema(
+							implementation = Laptime.class
+						)
+					)
+				)
+			),
+			@ApiResponse(responseCode = "400",
+				description = "Invalid request"
+			)
+		}
+	)
 	@PostMapping("/bestByTrack")
 	public ResponseEntity<List<Laptime>> getAllBestLaptimesByTrack(@RequestBody SearchDTO body)
 	{
@@ -44,5 +98,33 @@ public class LaptimeController
 			throw new NotFoundException("No entries found");
 		}
 		return ResponseEntity.ok().body(results);
+	}
+
+	@Operation(description = "Import laptimes",
+		requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+			content = @Content(
+				array = @ArraySchema(
+					schema = @Schema(
+						implementation = Laptime.class
+					)
+				)
+			)
+		),
+		responses = {
+			@ApiResponse(responseCode = "200",
+				description = "Operation Success"
+			),
+			@ApiResponse(responseCode = "400",
+				description = "Invalid request"
+			)
+		}
+	)
+	@PostMapping("/import")
+	public void importLaptimes(@RequestBody List<Laptime> laptimes)
+	{
+		if(laptimes==null){
+			throw new IllegalArgumentException("Null body");
+		}
+		laptimeRepository.saveAll(laptimes);
 	}
 }
